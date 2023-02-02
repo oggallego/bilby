86782357438e61906b9a4e620fc757700d429d73
lauragabellini <115634470+lauragabellini@users.noreply.github.com>
2023-02-02 10:43:43 +0000

    Update source.py

--- bilby/gw/source.py
+++ bilby/gw/source.py
@@ -13,7 +13,7 @@ from .utils import (lalsim_GetApproximantFromString,
 
 def lal_binary_black_hole(
         frequency_array, mass_1, mass_2, luminosity_distance, a_1, tilt_1,
-        phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, **kwargs):
+        phi_12, a_2, tilt_2, phi_jl, theta_jn, phase, ZeroParameter, **kwargs):
     """ A Binary Black Hole waveform model using lalsimulation
 
     Parameters
@@ -81,13 +81,13 @@ def lal_binary_black_hole(
         waveform_approximant='IMRPhenomPv2', reference_frequency=50.0,
         minimum_frequency=20.0, maximum_frequency=frequency_array[-1],
         catch_waveform_errors=False, pn_spin_order=-1, pn_tidal_order=-1,
-        pn_phase_order=-1, pn_amplitude_order=0, ZeroParameter=0)
+        pn_phase_order=-1, pn_amplitude_order=0)
     waveform_kwargs.update(kwargs)
     return _base_lal_cbc_fd_waveform(
         frequency_array=frequency_array, mass_1=mass_1, mass_2=mass_2,
         luminosity_distance=luminosity_distance, theta_jn=theta_jn, phase=phase,
         a_1=a_1, a_2=a_2, tilt_1=tilt_1, tilt_2=tilt_2, phi_12=phi_12,
-        phi_jl=phi_jl, **waveform_kwargs)
+        phi_jl=phi_jl, ZeroParameter=ZeroParameter, **waveform_kwargs)
 
 
 def lal_binary_neutron_star(
